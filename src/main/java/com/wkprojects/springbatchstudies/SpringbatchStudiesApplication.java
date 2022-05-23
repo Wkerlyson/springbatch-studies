0b97b2840604933cca24579581228d1c2584320a
Wkerlyson Batista <wkerlyson.batista@gmail.com>
2022-05-23 19:41:48 -0300

    first commit

---
+++ src/main/java/com/wkprojects/springbatchstudies/SpringbatchStudiesApplication.java
@@ -0,0 +1,13 @@
+package com.wkprojects.springbatchstudies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbatchStudiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbatchStudiesApplication.class, args);
+	}
+
+}
